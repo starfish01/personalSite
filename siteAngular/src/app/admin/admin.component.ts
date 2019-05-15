01f5a37d0893b2f58a6f39641df1a9fca4fca53b
starfish01 <patrick.labes@gmail.com>
2019-05-16 07:07:49 +1000

    added admin theme to admin

--- siteAngular/src/app/admin/admin.component.ts
+++ siteAngular/src/app/admin/admin.component.ts
@@ -2,20 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/core/auth.service';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss', './dist/css/skins/_all-skins.min.css', './dist/css/AdminLTE.min.css'
+  , "./bower_components/bootstrap/dist/css/bootstrap.min.css"
+    , "./bower_components/font-awesome/css/font-awesome.min.css"
+    , "./bower_components/Ionicons/css/ionicons.min.css"
+    , "./bower_components/jvectormap/jquery-jvectormap.css"
+    ]
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private authService:AuthService,private router: Router,  ) { }
+
+
+
+  constructor(private authService: AuthService, private router: Router, ) { }
 
   ngOnInit() {
   }
 
-  logout(){
-    this.authService.doLogout().then((data)=>{
+  logout() {
+    this.authService.doLogout().then((data) => {
       this.router.navigate(['/login']);
     });
   }
